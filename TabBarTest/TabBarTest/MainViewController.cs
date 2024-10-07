396e1542a1d6ee408f5081dc7831671c1b75ca7d
Tim Miller <innerlogic4321@gmail.com>
2024-10-07 10:46:12 +0900

    tabs

---
+++ TabBarTest/TabBarTest/MainViewController.cs
@@ -0,0 +1,105 @@
+namespace TabBarTest;
+
+public sealed class MainViewController : UITabBarController
+{
+    public MainViewController()
+    {
+        // You can either set tabs with UITabs, or use the View Controllers, but not both.
+        // Setting UITabs overrides ViewControllers.
+        
+        this.TabGroupSetup();
+        
+        //this.SetTabs();
+        
+        // this.SetViewControllers();
+
+        this.Mode = UITabBarControllerMode.TabSidebar;
+        this.Sidebar.PreferredLayout = UITabBarControllerSidebarLayout.Tile;
+        
+        var test = UIListContentConfiguration.HeaderConfiguration;
+        test.Text = "Test";
+        test.Image = UIImage.GetSystemImage("star.fill");
+
+        var bottomTest = UIListContentConfiguration.FooterConfiguration;
+        bottomTest.Text = "Bottom Test";
+        bottomTest.Image = UIImage.GetSystemImage("star.fill");
+        bottomTest.SecondaryText = "Secondary Text";
+        this.Sidebar.HeaderContentConfiguration = test;
+        this.Sidebar.FooterContentConfiguration = bottomTest;
+        this.Sidebar.BottomBarView = new UIView(new CGRect(0, 0, 100, 400))
+        {
+            BackgroundColor =  UIColor.Red
+        };
+    }
+
+    private void TabGroupSetup()
+    {
+        var testTab = new UITab[]
+        {
+            new UITab("Test 1", UIImage.GetSystemImage("star.fill"), "4", (tab => new TestViewController("Test 1"))),
+            new UITab("Test 3", UIImage.GetSystemImage("star.fill"), "6", (tab => new TestViewController("Test 3")))
+        };
+        
+        var testTab2 = new UITab[]
+        {
+            new UITab("Test 2", UIImage.GetSystemImage("star.fill"), "5", (tab => new TestViewController("Test 2")))
+        };
+        
+        var tabGroups = new UITabGroup("Test", UIImage.GetSystemImage("star.fill"), "test2", testTab, (group
+            =>
+        {
+            var test = new TestViewController("Section Group 3");
+            return test;
+        }));
+        var tabGroups2 = new UITabGroup("Test2", UIImage.GetSystemImage("star.fill"), "test3", testTab2, (group => new TestViewController("Section Group 2")));
+        
+        this.Tabs = new UITab[]
+        {
+            new UITab("Fart", UIImage.GetSystemImage("star.fill"), "Fart", (tab => new TestViewController("Fart"))),
+            tabGroups2,
+            tabGroups,
+            new UITab("Poop", UIImage.GetSystemImage("star.fill"), "Poop", (tab => new TestViewController("Fart"))),
+        };
+    }
+
+    private void SetTabs()
+    {
+        var testTab = new UITab[]
+        {
+            new UITab("Test 1", UIImage.GetSystemImage("star.fill"), "50", (tab => new TestViewController("Test 1")))
+        };
+        
+        this.Tabs = testTab;
+    }
+
+    private void SetViewControllers()
+    {
+        var tabs = new UIViewController[]
+        {
+            new TestViewController("Favorites") { TabBarItem = new UITabBarItem(UITabBarSystemItem.Favorites, 0) },
+            new TestViewController("Bookmarks") { TabBarItem = new UITabBarItem(UITabBarSystemItem.Bookmarks, 1) },
+            new TestViewController("History") { TabBarItem = new UITabBarItem(UITabBarSystemItem.History, 2) },
+        };
+        
+        this.ViewControllers = tabs;
+        this.SelectedViewController = tabs[1];
+    }
+}
+
+public sealed class TestViewController : UIViewController
+{
+    public TestViewController(string title)
+    {
+        var label = new UILabel(new CGRect(0, 0, 300, 40))
+        {
+            Text = title,
+            TextAlignment = UITextAlignment.Center
+        };
+        this.View!.AddSubview(label);
+        
+        this.Title = title;
+        
+        label.AutoresizingMask = UIViewAutoresizing.All;
+        label.Center = this.View!.Center;
+    }
+}
