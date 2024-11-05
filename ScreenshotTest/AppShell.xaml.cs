7eae53c8d051d83d423ebdffeddb9ee3e84725ce
Timothy Miller <innerlogic4321@gmail.com>
2024-11-05 16:30:30 +0900

    Screenshot

---
+++ ScreenshotTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ScreenshotTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ScreenshotTest/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace ScreenshotTest;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		var result = await Screenshot.CaptureAsync();
+		var fileStream = await result.OpenReadAsync();
+		var image = new Image { Source = ImageSource.FromStream(() => fileStream) };
+		DotNetBotImage.Source = image.Source;
+	}
+}
+
