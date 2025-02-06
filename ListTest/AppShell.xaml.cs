99ea116720fffa1935823eac0a89b9c35869be31
Tim Miller <innerlogic4321@gmail.com>
2025-02-06 23:25:23 +0900

    Commit

---
+++ ListTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ListTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ListTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ListTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
