1a2bd301700323b2b23bd7fd14b8210a70d3dbde
Tim Miller <innerlogic4321@gmail.com>
2025-03-27 11:25:28 +0900

    iOS

---
+++ TestApp/HotReloadService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: System.Reflection.Metadata.MetadataUpdateHandlerAttribute(typeof(TestApp.HotReloadService))]
+namespace TestApp
+{
+    public static class HotReloadService
+    {
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        public static event Action<Type[]?>? UpdateApplicationEvent;
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+
+        internal static void ClearCache(Type[]? types) { }
+        internal static void UpdateApplication(Type[]? types)
+        {
+            UpdateApplicationEvent?.Invoke(types);
+        }
+    }
+}
---
+++ TestApp/MainViewController.cs
@@ -0,0 +1,50 @@
+﻿using Masonry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class MainViewController : UIViewController
+    {
+        public MainViewController()
+        {
+            HotReloadService.UpdateApplicationEvent += OnHotReload;
+        }
+
+        private void OnHotReload(Type[]? obj)
+        {
+            this.InvokeOnMainThread(ViewDidLoad);
+        }
+
+        public override void ViewDidLoad()
+        {
+            this.View = new UIView();
+            Build();
+        }
+
+        private void Build()
+        {
+            this.View!.BackgroundColor = UIColor.SystemBackground;
+            var button = UIButton.FromType(UIButtonType.System);
+            var uiButtonConfiguration = UIButtonConfiguration.FilledButtonConfiguration;
+            button.Configuration = uiButtonConfiguration;
+            button.SetTitle("Click me", UIControlState.Normal);
+            button.TouchUpInside += (sender, e) =>
+            {
+                //var alert = UIAlertController.Create("Hello", "Hello, World!", UIAlertControllerStyle.Alert);
+                //alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                //this.PresentViewController(alert, true, null);
+            };
+
+            this.View.AddSubview(button);
+            button.MakeConstraints((make) =>
+            {
+                make.CenterX.EqualTo(this.View.CenterX());
+                make.CenterY.EqualTo(this.View.CenterY());
+            });
+        }
+    }
+}
